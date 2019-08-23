2d1034109d960b66397509f99fd25221db2ce866
antimage <dangtung789.td@gmail.com>
2019-08-23 10:43:59 +0700

    them chuoi

--- quantumtest.py
+++ quantumtest.py
@@ -5,3 +5,16 @@ print(type(a))
 from fractions import*
 b=Fraction(3,4)
 print(b)
+#chuoi 
+#cong choi 
+a=""" hoc"""
+print('chuoi ban dau la'+a)
+a=a+'hoc mai'
+print('chuoi sau khi cong la'+a)
+b=' hoc mai'
+c=a+"\n"+b
+e=b in c
+print(e)	
+
+print(a[1])
+
